da3986895879d7dc04359a8899ea0e7356da551a
islem <islemomrani1005@gmail.com>
2025-10-19 01:07:51 -1100

    ajout d'un espace club

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { Routes } from '@angular/router';
 import { EventCreateComponent } from './components/event-create/event-create.component';
 import { EventDetailComponent } from './components/event-detail/event-detail.component';
 import { EventListComponent } from './components/event-list/event-list.component';
+import { CreateClubComponent } from './components/clubs/create-club/create-club.component';
+import { EspaceClubComponent } from './components/clubs/espace-club/espace-club.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -12,5 +14,9 @@ export const routes: Routes = [
   { path: 'events', component: EventListComponent },
   { path: 'events/create', component: EventCreateComponent },
   { path: 'events/:id', component: EventDetailComponent },
+  {path: 'createClub ',component:CreateClubComponent },
+//il faut  avec id 
+ //  {path: 'espaceClub/:id',component:EspaceClubComponent },
+  {path: 'espaceClub',component:EspaceClubComponent },
 ]
 
