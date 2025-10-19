63d6321b28acd95f377485ac8449b53a7a7eccd3
eyya <dhaouieya00@gmail.com>
2025-10-19 17:47:43 +0100

    consulter la liste des clubs , header , footer

---
+++ Front/src/app/components/clubs/clubs-list/clubs-list.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Club } from '../../../models/club';
+import { ClubService } from '../../../services/club.service';
+import { HeaderComponent } from "../../header/header.component";
+import { FooterComponent } from "../../footer/footer.component";
+
+@Component({
+  selector: 'app-clubs-list',
+  imports: [HeaderComponent, FooterComponent],
+  templateUrl: './clubs-list.component.html',
+  styleUrl: './clubs-list.component.css',
+})
+export class ClubsListComponent implements OnInit {
+  clubs: Club[] = [];
+
+  constructor(private clubService: ClubService) {}
+
+  ngOnInit(): void {
+    this.loadClubs();
+  }
+
+  loadClubs(): void {
+    this.clubService.getClubs().subscribe(
+      (response: any) => {
+        if (response.success) {
+          this.clubs = response.data;
+        } else {
+          console.error('Failed to load clubs');
+        }
+      },
+      (err: any) => {
+        console.error('Error loading clubs:', err);
+      }
+    );
+  }
+
+
+}
--- back/controllers/clubController.js
+++ back/controllers/clubController.js
@@ -33,6 +33,18 @@ exports.getClubByManager = async (req, res) => {
   }
 };
 
+exports.getAllClubs = async (req, res) => {
+  try {
+    const clubs = await Club.find({ actif: true });
+    res.status(200).json(
+      { success: true, data: clubs }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Erreur lors de la récupération des clubs actifs' });
+  }
+}
+
 
 
 
