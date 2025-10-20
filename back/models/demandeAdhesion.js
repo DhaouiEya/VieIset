acfd3cef985186604a1ab826932e9125a2dab38f
islem <islemomrani1005@gmail.com>
2025-10-20 07:19:05 -1100

    regler conflit

---
+++ back/models/demandeAdhesion.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const demandeAdhesionSchema = new mongoose.Schema({
+    etudiant: { type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Etudiant',
+         required: true },
+    club: { type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Club', 
+        required: true },
+    statut: { type: String, enum: ['en attente','en cours','acceptée', 'refusée'], default: 'en attente' },
+    dateDemande: { type: Date, default: Date.now },
+    datesProposees: [{ type: Date }],   
+    dateChoisie: { type: Date }  
+},{timestamps:true});
+
+module.exports = mongoose.model('DemandeAdhesion', demandeAdhesionSchema);
--- back/routes/index.js
+++ back/routes/index.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const posteRoutes = require('./posteRoutes');
 const authRoutes =  require('./authRoutes');
 const clubRoutes=require('./clubRoutes');
+const demandeAdhesionRoutes=require('./demandeAdhesionRoutes');
 
 
 router.use('/postes', posteRoutes);
 router.use('/auth', authRoutes);
 router.use('/clubs', clubRoutes);
+router.use('/demandeAdhesion', demandeAdhesionRoutes);
 
 
 module.exports=router;
